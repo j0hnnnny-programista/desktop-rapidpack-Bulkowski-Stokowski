2244c2037e84f0cc781e5376d3d45995f36a2297
bezGrandy <wojciech.stokowski@cyfroweszkoly.pl>
2026-03-10 00:48:09 +0100

    add CalculatePrice method to ParcelCalculator

--- RapidPack/Services/ParcelCalculator.cs
+++ RapidPack/Services/ParcelCalculator.cs
@@ -1,6 +1,15 @@
+using System;
+
 namespace RapidPack.Services;
 
 public class ParcelCalculator
 {
-    
+    public decimal CalculatePrice(decimal height, decimal width, decimal depth, decimal weight,
+        bool express, string packageType)
+    {
+        decimal TotalPrice = 0;
+        
+
+        return TotalPrice;
+    }
 }
