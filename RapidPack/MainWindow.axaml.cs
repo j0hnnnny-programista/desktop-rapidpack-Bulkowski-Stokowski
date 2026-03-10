401ad64ff3b6d2afda0fcdf5b0ab6a90226f9c9b
bezGrandy <wojciech.stokowski@cyfroweszkoly.pl>
2026-03-10 01:08:03 +0100

    create calculation method in MainWindow and check inputs

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -1,11 +1,31 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using RapidPack.Services;
 
-namespace RapidPack;
-
-public partial class MainWindow : Window
+namespace RapidPack
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public ParcelCalculator calculator = new ParcelCalculator();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void calculation(object? sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!int.TryParse(PackHeight.Text, out int height) ||
+                    !int.TryParse(PackWidth.Text, out int width) ||
+                    !int.TryParse(PackDepth.Text, out int depth) ||
+                    !int.TryParse(PackWeight.Text, out int weight))
+                {
+                    TotalPrice.Text = "Błąd: wpisz liczby!";
+                    return;
+                }
+        }
     }
 }
